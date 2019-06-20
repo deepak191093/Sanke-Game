cc2f03f54ff05d74991b459a5479bd8fecff4a9b
deepak191093 <deepak191093@gmail.com>
2019-06-20 23:37:29 +0530

    Static Snake and PlayGround Setup

--- src/components.js/gameContainer.js
+++ src/components.js/gameContainer.js
@@ -1,21 +1,59 @@
 import React from "react";
 import foodGenerator from "./foodGenerator";
+import Snake from "./snake";
 class GameContainer extends React.Component{
     constructor(props)
     {
         super(props);
         this.state = {
             foodLocation : foodGenerator(),
+            snakeDots : [[0, 0], [1, 0]],
+            direction : 'RIGHT'
         };
     }
 
+    componentDidMount(){
+        document.onkeydown = this.onDirection;
+    }
+
+    onDirection = (event) =>{
+        event = event ||window.event;
+        switch(event.keyCode)
+        {
+            case 37:
+                    this.setState({direction : 'LEFT'});
+                    console.log("LEFT")
+                    break;
+            case 38:
+                    this.setState({direction : 'UP'});
+                    console.log("UP");
+                    break;
+            case 39:
+                    this.setState({direction : 'RIGHT'});
+                    console.log("RIGHT");
+                    break;
+            case 40:
+                    this.setState({direction : 'DOWN'});
+                    console.log("DOWN");
+                    break;
+        }
+
+    }
     render()
     {
         return(
-            <div className="gameContainer">
-                <div className="snake-dots-1" style={{top:"40px", left:"100px"}}></div>
-                <div className="snake-dots-2" style={{top:"20px", left:"100px"}}></div>
-                <div className="food" style = {{ top:this.state.foodLocation[0]*20+"px",  left:this.state.foodLocation[1]*20+"px"}} ></div>
+            <div>
+            <span id="span-1">
+                <img src="./resources/images/logo.png" id="logo"/>
+                <p className="YOUR-SCORE">YOUR SCORE</p>
+                <div className="gameContainer">
+                    <Snake snakeDots={this.state.snakeDots} />
+                    <div className="food" style = {{ top:this.state.foodLocation[0]*20+"px",  left:this.state.foodLocation[1]*20+"px"}} ></div>
+                </div>
+            </span>
+            <span id="span-2">
+
+            </span>
             </div>
         );
     }
--- src/components.js/snake.js
+++ src/components.js/snake.js
@@ -1,6 +1,6 @@
-import {Component} from "react";
+import React from "react";
 
-class Snake extends Component{
+class Snake extends React.Component{
     constructor(props)
     {
         super(props);
@@ -9,7 +9,15 @@ class Snake extends Component{
     render()
     {
         return(
-            
+            <div>
+                {
+                    this.props.snakeDots.map((dot, i=0)=>{
+                    return (<div className={i%2==0 ?"snake-dots-1" : "snake-dots-2"} style={{left: `${dot[0]*20}px`, top: `${dot[1]*20}px` }} key={i++} />)
+                    })
+                }
+            </div>
         );
     }
 }
+
+export default Snake;
