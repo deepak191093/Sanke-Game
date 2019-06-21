f98d06d6d066ecc1de5c62212a8f2d523c7f9b29
deepak191093 <deepak191093@gmail.com>
2019-06-22 00:25:51 +0530

    Snake Game Working

--- src/components.js/gameContainer.js
+++ src/components.js/gameContainer.js
@@ -1,12 +1,13 @@
 import React from "react";
 import foodGenerator from "./foodGenerator";
 import Snake from "./snake";
+
 class GameContainer extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       foodLocation: foodGenerator(),
-      snakeDots: [[0, 0], [1, 0], [2, 0],[3, 0], [4, 0]],
+      snakeDots: [[0, 0], [1, 0], [2, 0]],
       direction: "RIGHT"
     };
     this.onDirection = this.onDirection.bind(this);
@@ -16,37 +17,38 @@ class GameContainer extends React.Component {
     this.hitItself = this.hitItself.bind(this);
     this.eatFood = this.eatFood.bind(this);
   }
-
+//---------------------------Life-Cycles-Method----------------------------------//
   componentDidMount() {
-    setInterval(this.snakeMovement, 200);
+    setInterval(this.snakeMovement, 100);
     document.onkeydown = this.onDirection;
   }
 
-  
-  componentDidUpdate()
-  {
+  componentDidUpdate() {
     this.hitItself();
     this.boundry();
-    
+    this.eatFood();
   }
+//---------------------------Life-Cycles-Method----------------------------------//
+
+//---------------------------General-Methods------------------------------------//
   onDirection = event => {
     event = event || window.event;
     switch (event.keyCode) {
       case 37:
-        this.setState({ direction: "LEFT" });
-        console.log("LEFT");
+        if(this.state.direction!=="RIGHT")
+          this.setState({ direction: "LEFT" });
         break;
       case 38:
+        if(this.state.direction!=="DOWN")
         this.setState({ direction: "UP" });
-        console.log("UP");
         break;
       case 39:
+        if(this.state.direction!=="LEFT")
         this.setState({ direction: "RIGHT" });
-        console.log("RIGHT");
         break;
       case 40:
+        if(this.state.direction!=="UP")
         this.setState({ direction: "DOWN" });
-        console.log("DOWN");
         break;
     }
   };
@@ -73,31 +75,46 @@ class GameContainer extends React.Component {
     this.setState({ snakeDots: co_ordinates });
   }
 
-  boundry(){ 
-    let head = this.state.snakeDots[this.state.snakeDots.length -1];
+  boundry() {
+    let head = this.state.snakeDots[this.state.snakeDots.length - 1];
     if (head[0] >= 25 || head[1] >= 25 || head[0] < 0 || head[1] < 0) {
       this.onGameOver();
     }
-  } 
-  
-  onGameOver()
-  {
+  }
+
+  onGameOver() {
     alert("Game Over");
-    this.setState({snakeDots: [[0, 0], [1, 0], [2, 0],[3,0],[4,0]],direction: "RIGHT"})
+    this.setState({
+      snakeDots: [[0, 0], [1, 0], [2, 0]],
+      direction: "RIGHT"
+    });
   }
 
-  hitItself()
-  {
+  hitItself() {
     let snake = [...this.state.snakeDots];
-    let head = snake[snake.length -1];
+    let head = snake[snake.length - 1];
     snake.pop();
-    snake.forEach((dot) => {
-      if(head[0] == dot[0] && head[1] == dot[1])
-      {
+    snake.forEach(dot => {
+      if (head[0] == dot[0] && head[1] == dot[1]) {
         this.onGameOver();
-      }}) 
+      }
+    });
   }
 
+  eatFood() {
+    let food = this.state.foodLocation;
+    let head = this.state.snakeDots[this.state.snakeDots.length - 1];
+    let newSnake = [...this.state.snakeDots];
+    console.log(food, "  ", head);
+    if (head[0] == food[0] && head[1] == food[1]) {
+      this.setState({ foodLocation: foodGenerator() });
+      newSnake.unshift([]);
+      this.setState({ snakeDots: newSnake });
+    }
+  }
+  //---------------------------General-Methods------------------------------------//
+
+  //---------------------------Render-Methods------------------------------------//
   render() {
     return (
       <div>
@@ -109,8 +126,8 @@ class GameContainer extends React.Component {
             <div
               className="food"
               style={{
-                top: this.state.foodLocation[0] * 20 + "px",
-                left: this.state.foodLocation[1] * 20 + "px"
+                left: `${this.state.foodLocation[0] * 20}px`,
+                top: `${this.state.foodLocation[1] * 20}px`
               }}
             ></div>
           </div>
